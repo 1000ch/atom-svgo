4b286789d27bfe7a4d5ad61990184ff65e16a7b1
1000ch <shogo.sensui@gmail.com>
2017-03-05 16:27:10 +0900

    Use execa

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 import { type } from 'os';
 import { normalize, join } from 'path';
-import { spawn } from 'child_process';
+import execa from 'execa';
 
 const unix = normalize(join(__dirname, 'node_modules', '.bin', 'svgo'));
 const win = normalize(join(__dirname, 'node_modules', '.bin', 'svgo.cmd'));
-const SVGO_PATH = type() === 'Windows_NT' ? win : unix;
+const svgo = type() === 'Windows_NT' ? win : unix;
 
 function minify(pretty = false) {
   const editor = atom.workspace.getActiveTextEditor();
@@ -15,35 +15,19 @@ function minify(pretty = false) {
     return;
   }
 
-  let args = ['--input', editor.getPath(), '--output', '-'];
+  const args = ['--string', editor.getText(), '--output', '-'];
   if (pretty) {
     args.push('--pretty');
   }
 
-  let chunks = [];
-  let errors = [];
-
-  let cp = spawn(SVGO_PATH, args);
-  cp.stdout.on('data', chunk => {
-    chunks.push(chunk);
-  });
-
-  cp.stderr.on('data', error => {
-    errors.push(error);
-  });
-
-  cp.on('error', error => {
-    atom.notifications.addError(error, {});
-  });
-
-  cp.on('exit', code => {
-    if (code === 2) {
-      atom.notifications.addError(Buffer.concat(errors).toString(), {});
-    } else {
-      let position = editor.getCursorBufferPosition();
-      editor.setText(Buffer.concat(chunks).toString());
-      editor.setCursorBufferPosition(position);
-    }
+  execa.stdout(svgo, args, {
+    encoding: null
+  }).then(stdout => {
+    const position = editor.getCursorBufferPosition();
+    editor.setText(stdout.toString());
+    editor.setCursorBufferPosition(position);
+  }).catch(error => {
+    atom.notifications.addError(errors, {});
   });
 }
 
